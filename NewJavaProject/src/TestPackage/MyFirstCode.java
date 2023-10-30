bf36cae3850d4ad47b5b2834bf3d548b01da482b
LENOVO <LENOVO@DESKTOP-V7A1CDP>
2023-10-30 20:45:14 +0530

    First Commit

---
+++ NewJavaProject/src/TestPackage/MyFirstCode.java
@@ -0,0 +1,22 @@
+package TestPackage;
+
+public class MyFirstCode {
+	
+
+	public static void main(String[] args) {
+		
+		int num=11;
+		//while(num>0) {
+			if(num%2==0) {
+				System.out.println("even number");
+			}
+			else {
+				System.out.println("odd number");
+			}
+			//break;
+		}
+
+	}
+
+
+
